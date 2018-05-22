c1d5dd6a5b655216958f11294d42638921dfa7ad
Ruth Rutter <rarutter@alaska.edu>
2018-05-22 13:39:36 -0800

    Adds modified run script for experimenting on Chinook

---
+++ run_ddt_ch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+
+#SBATCH --mail-user=rarutter@alaska.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# Job name, for clarity
+#SBATCH --job-name="ddt50squarePoC"
+
+# Time limit (since we're requesting a lot of resources)
+#SBATCH --time=12:00:00
+
+# Partition specification
+#SBATCH -p t2standard 
+
+# Number of MPI tasks
+#SBATCH -n 1250 
+
+#    SBATCH --test-only
+
+ulimit -s unlimited
+ulimit -l unlimited
+
+. /etc/profile.d/modules.sh
+module purge
+module load slurm
+
+echo $SLURM_JOB_NODELIST
+
+# srun -l /bin/hostname | sort -n | awk '{print $2}' > ./nodes.$SLURM_JOB_ID
+
+# mpirun -n 1 -machinefile ./nodes.$SLURM_JOB_ID ./dvmdostem -l disabled -p 100 -e 1000 -s 250 -t 109 -n 91 
+mpirun -n 1250 ./dvmdostem -l disabled -p 100 -e 1000 -s 250 -t 109 -n 91 
